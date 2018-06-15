fd4e54fa02f9709bf71a5153d0a628f2421ee3c3
Adele Bendayan <adele.bendayan@gmail.com>
2018-06-15 11:41:20 +0300

    add features

--- video_extract.py
+++ video_extract.py
@@ -55,8 +55,6 @@ class UCSD:
                 features = hs.tolist()
                 features.extend([f_cnt, atom_mag, i, j, tag])
                 features_j.append(features[:-1])
-                if len(features[:-1]) < 13:
-                    import pdb; pdb.set_trace()
                 labels_j.append(tag)
                 for f in features:
                     out.write(str(f) + " ")
@@ -67,7 +65,7 @@ class UCSD:
         mag_threshold=1e-3
         elements = 0
         is_tagged = not tag_video == ""
-        out = open("features/features_test_"+type+"_"+video_name.split("/")[0]+".txt","w")
+        out = open("features/features_test_"+type+"_"+video_name.split("/")[1]+".txt","w")
         files = [f for f in listdir(self.path+video_name) if isfile(join(self.path+video_name, f))]
         if is_tagged:
             files_tag = [f for f in listdir(self.path+tag_video) if isfile(join(self.path+tag_video, f))]
@@ -145,28 +143,28 @@ def load_train_features(type):
 
 if __name__ == '__main__':
     ucsdped = 'UCSDped1'
-    # ucsd_training = UCSD('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Train/', 10, 5)
-    # dir_trains = [f for f in listdir('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Train/') if isdir(join('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Train/', f))]
-    # dir_tests = [f for f in listdir('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Test/') if isdir(join('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Test/', f))]
-    # dir_trains.sort()
-    # # out = open("features_test_UCSDped1.txt","w")
-    # # ucsd_training.extract_features('Train001/', ucsdped)
-    # dir_trains.pop(0)
-    #
-    # # li = ["Test/Test003"]
-    # li = ["Test/Test003","Test/Test004","Test/Test014","Test/Test018","Test/Test019", "Test/Test021","Test/Test022","Test/Test023","Test/Test024","Test/Test032"]
-    # ucsd_training.path = 'UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/'
-    # for directory in li:
-    #     print directory
-    #     if not directory.endswith("gt"):
-    #         dir_split = directory.split("/")[1]
-    #         dir_split = dir_split + '_gt'
-    #         if dir_split in dir_tests:
-    #             ucsd_training.extract_features(directory+'/', ucsdped, directory + '_gt/')
-    #         else:
-    #             ucsd_training.extract_features(directory+'/', ucsdped)
+    ucsd_training = UCSD('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Train/', 10, 5)
+    dir_trains = [f for f in listdir('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Train/') if isdir(join('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Train/', f))]
+    dir_tests = [f for f in listdir('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Test/') if isdir(join('UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/Test/', f))]
+    dir_trains.sort()
+    # out = open("features_test_UCSDped1.txt","w")
+    # ucsd_training.extract_features('Train001/', ucsdped)
+    dir_trains.pop(0)
+
+    # li = ["Test/Test003"]
+    li = ["Test/Test003","Test/Test004","Test/Test014","Test/Test018","Test/Test019", "Test/Test021","Test/Test022","Test/Test023","Test/Test024","Test/Test032"]
+    ucsd_training.path = 'UCSD_Anomaly_Dataset.v1p2/'+ucsdped+'/'
+    for directory in li:
+        print directory
+        if not directory.endswith("gt"):
+            dir_split = directory.split("/")[1]
+            dir_split = dir_split + '_gt'
+            if dir_split in dir_tests:
+                ucsd_training.extract_features(directory+'/', ucsdped, directory + '_gt/')
+            else:
+                ucsd_training.extract_features(directory+'/', ucsdped)
 
     x_train, target = load_train_features(ucsdped)
-    learning = VideoLearn(13, 1)
+    learning = VideoLearn(13, 5, 0.001)
 
-    learning.learn(x_train, target, 1)
+    learning.learn(x_train, target, 50)
